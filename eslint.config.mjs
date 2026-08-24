4d6301f77535117571199b95d9c15cabb5ab3d35
AlaaMousa05 <loloadnan.2024@gmail.com>
2026-08-24 21:37:49 +0300

    Update project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "test-results/**",
+      "playwright-report/**",
+      "next-env.d.ts",
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
